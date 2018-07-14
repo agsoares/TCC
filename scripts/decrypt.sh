9c9c069cb913fbc843fbab62f6fe8c29db81f6dc
Adriano Soares <gsoares.adriano@gmail.com>
2018-07-14 17:20:21 -0300

    correct scripts for decryption and encryption

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-if [ ! -f .env ]
-then
-  export $(cat .env | xargs)
-fi
+[ -a .env ] && export $(cat .env | xargs)
 
 openssl enc -aes-256-cbc -d -in Sources/Application/GoogleService-Info.plist.enc -out Sources/Application/GoogleService-Info.plist -k $PASSWORD_ENCRYPT
--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,8 +1,5 @@
 #! /usr/bin/env sh
 
-if [ ! -f .env ]
-then
-  export $(cat .env | xargs)
-fi
+[ -a .env ] && export $(cat .env | xargs)
 
 openssl enc -aes-256-cbc -in Sources/Application/GoogleService-Info.plist -out Sources/Application/GoogleService-Info.plist.enc -k $PASSWORD_ENCRYPT
